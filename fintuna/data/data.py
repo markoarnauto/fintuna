b1526aa26ba7e0f4972b56e5a6ed927a9864dc97
Markus Tretzmüller <markus.tretzmueller@cortecs.at>
2022-10-05 13:01:08 +0200

    use non-transformed/compressed btcpairs_with_social_sentiment data

--- fintuna/data/data.py
+++ fintuna/data/data.py
@@ -2,6 +2,9 @@
 import os
 
 import pandas as pd
+import numpy as np
+from fintuna.utils import lagged_features
+
 
 # High-dimensional features of BTC-pairs including social-sentiment from cortecs.ai
 def get_btcpairs_with_social_sentiment() -> tuple:
@@ -11,7 +14,22 @@ def get_btcpairs_with_social_sentiment() -> tuple:
         "period": "24h",
         "sampling_freq": "3h"
     }
-    data = pd.read_hdf(os.path.dirname(__file__) + '/btcpairs_with_social_sentiment.h5')
+    data = pd.read_hdf(os.path.dirname(__file__) + '/btcpairs_with_social_sentiment.h5')  # type: pd.DataFrame
+    ## preprocessing
+    # lagging those features makes less sense
+    features_to_exclude = ['trading_volume__max', 'spread', 'rsi__6h', 'trading_volume__ema_6h',
+     'longterm_return', 'overall_longterm_return', 'longterm_volatility',
+     'longterm_skew', 'overall_twitter_activity__ema_6h', 'high', 'low']
+    data_transf = data.drop(features_to_exclude, axis=1, level=1)
+    data_not_transf = data.loc[:, (slice(None), features_to_exclude)]
+    transformed_data = lagged_features(data_transf, specs['period'], 20)
+    data = pd.concat([data_not_transf, transformed_data], axis=1)
+
+    ## do some post processing
+    # clip initiation data
+    data = data[pd.Timestamp('2020-01-01', tz='utc') + pd.Timedelta(specs['period']):]
+    # treat inf as nan
+    data = data.replace([np.inf, -np.inf], np.nan)
     return data, specs
 
 # simple altcoins features: lagged returns and zscored trading volume
